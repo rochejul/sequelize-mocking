2934272bb74c8d4a2ac4e8e19df8ff86fc715b1a
Julien Roche (SESA434942) <julien.roche@viseo.com>
2016-10-26 11:10:36 +0200

    Update to be full compatible with Jasmine

---
+++ lib-es5/sequelize-mocking-jasmine.js
@@ -0,0 +1,49 @@
+/**
+ * Using SequelizeMocking with jasmine easily
+ *
+ * @module lib/sequelize-mocking-jasmine
+ * @exports sequelizeMochingJasmine
+ * @version 0.1.0
+ * @since 0.1.0
+ * @author Julien Roche
+ */
+
+'use strict';
+
+// Imports
+
+var Sequelize = require('sequelize');
+var _ = Sequelize.Utils._;
+var SequelizeMocking = require('./sequelize-mocking');
+
+/**
+ * @name sequelizeMochingMocha
+ * @param {Sequelize} originalSequelize
+ * @param {string} [fixtureFilePath]
+ * @param {SequelizeMockingOptions} [options]
+ */
+module.exports = function (originalSequelize, fixtureFilePath, options) {
+    var beforeEach = global.beforeEach ? global.beforeEach : null;
+    var afterEach = global.afterEach ? global.afterEach : null;
+
+    if (beforeEach && afterEach) {
+        (function () {
+            var mockedSequelize = null;
+
+            beforeEach(function (done) {
+                var createFunc = _.partialRight(fixtureFilePath ? SequelizeMocking.createAndLoadFixtureFile : SequelizeMocking.create, options);
+
+                createFunc(originalSequelize, fixtureFilePath).then(function (sequelizeInstance) {
+                    mockedSequelize = sequelizeInstance;
+                    done();
+                }).catch(done.fail);
+            });
+
+            afterEach(function (done) {
+                SequelizeMocking.restore(mockedSequelize, options).then(function () {
+                    done();
+                }).catch(done.fail);
+            });
+        })();
+    }
+};
